822986a339c01ce2060ba658cfcb46c4f17076b0
Kateryna Tkachenko <Tkachenko.city@gmail.com>
2023-12-08 23:19:17 +0200

    generic ready

--- src/generic/8.ts
+++ src/generic/8.ts
@@ -3,22 +3,29 @@
   Ви хочете створити новий тип Params, який включає всі поля з Form, крім errors.
 */
 
-// type Errors = {
-//   email?: string[];
-//   firstName?: string[];
-//   lastName?: string[];
-//   phone?: string[];
-// };
+type Errors = {
+  email?: string[];
+  firstName?: string[];
+  lastName?: string[];
+  phone?: string[];
+};
 
-// type Form = {
-//   email: string | null;
-//   firstName: string | null;
-//   lastName: string | null;
-//   phone: string | null;
-//   errors: Errors;
-// };
+type Form = {
+  email: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  phone: string | null;
+  errors: Errors;
+};
 
-// // Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
-// type Params = Form;
+// Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
+type Params = Omit<Form, 'errors'>;
 
-// export {};
+const params: Params = {
+  email: 'example@gmail.com',
+  firstName: 'dfgdffdfff',
+  lastName: 'dfdfddfdf',
+  phone: '123456789',
+};
+
+export {};
